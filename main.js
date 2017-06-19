548a62e00aed5d6d1fc06e1e31e1e808d91a2d44
Nick Garvey <garvey.nick@gmail.com>
2017-06-18 23:22:47 -0700

    Remove unneeded killExcessCreeps

--- main.js
+++ main.js
@@ -2,29 +2,13 @@ const Creep_ = require('creep');
 const Extension_ = require('extension');
 const Spawn_ = require('spawn');
 
-function cleanUpOldCreeps() {
+function garbageCollect() {
     for (const name in Memory.creeps) {
         if (!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
-}
-
-function killExcessCreeps() {
-    const totalOver = _.size(Game.creeps) - CREEP_CAP;
-    if (totalOver <= 0) {
-        return;
-    }
-
-    _.chain(Game.creeps)
-        .sortBy((creep) => _.sum(creep.carry))
-        .slice(0, totalOver)
-        .value()
-        .forEach((c) => {
-            console.log('killing', c);
-            c.suicide();
-        });
 }
 
 function buildConstructionSites() {
@@ -35,10 +19,12 @@ function buildConstructionSites() {
 
 module.exports.loop = function() {
     console.log('START ticks available:', Game.cpu.tickLimit);
-    cleanUpOldCreeps();
+
     buildConstructionSites();
-    // killExcessCreeps();
     Spawn_.doSpawn();
     _.forEach(Game.creeps, Creep_.run);
+
+    garbageCollect();
+
     console.log('END   ticks used:', Math.ceil(Game.cpu.getUsed()));
 }
